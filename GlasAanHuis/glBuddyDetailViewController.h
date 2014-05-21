73536ef36088f91daca581fdfdca7c30c32625b0
valkelol <lmlwoezi@imac27-10.local>
2014-05-21 15:25:59 +0200

    chat

---
+++ GlasAanHuis/glBuddyDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  glBuddyDetailViewController.h
+//  GlasAanHuis
+//
+//  Created by leon van Woezik on 21/05/14.
+//  Copyright (c) 2014 GlasAanHuis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface glBuddyDetailViewController : UIViewController
+
+@end
